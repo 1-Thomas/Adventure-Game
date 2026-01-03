652f38d43d8dbe337f0cd4d4248eb378689f69ba
1-Thomas <10628936@bradfordcollege.ac.uk>
2026-01-03 21:31:01 +0000

    Health Potions

--- rpg.cpp
+++ rpg.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 class Player {
 public:
     std::string name;
@@ -15,8 +16,35 @@ public:
     Enemy(const std::string& n, int h, int a) : name(n), hp(h), attackD(a) {}
 };
 
+class Item {
+public:
+    std::string name, description;
+
+    Item(const std::string& n, const std::string& d) : name(n), description(d) {}
+    virtual ~Item() = default;
+    virtual void use(Player& player) = 0;
+};
+
+class HealthPotion : public Item {
+    int healAmount;
+public:
+    HealthPotion(int amt) : Item("Healing Potion", "Restores health."), healAmount(amt) {}
+    void use(Player& player) override {
+        player.hp += healAmount;
+        std::cout << "You gained " << healAmount << "HP.\n";
+    }
+};
+
+
 int main() { 
     std::string pname;
     std::cout << "Insert Player Name: ";
     std::cin >> pname;
+
+    Player p(pname);
+
+    std::cout << "Player " << p.name << " created with " << p.hp << "HP and " << p.attackD << " Attack Damage"<< "\n";
+    Item* potion = new HealthPotion(6);
+    potion->use(p);
+    delete potion;
 }
