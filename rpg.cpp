d7f238a3704fcade631c63ead559d62e9568771e
1-Thomas <10628936@bradfordcollege.ac.uk>
2026-01-03 20:42:08 +0000

    Enemy Class

--- rpg.cpp
+++ rpg.cpp
@@ -7,6 +7,14 @@ public:
     Player(const std::string& n) : name(n), hp(25), attackD(5) {}
 };
 
+class Enemy {
+public:
+    std::string name;
+    int hp, attackD;
+
+    Enemy(const std::string& n, int h, int a) : name(n), hp(h), attackD(a) {}
+};
+
 int main() { 
     std::string pname;
     std::cout << "Insert Player Name: ";
