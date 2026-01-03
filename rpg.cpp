ee1401da8ef590e909431c69ee874cabefdc4c92
1-Thomas <10628936@bradfordcollege.ac.uk>
2026-01-03 19:46:55 +0000

    Player Class

--- rpg.cpp
+++ rpg.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+class Player {
+public:
+    std::string name;
+    int hp, attackD;
+
+    Player(const std::string& n) : name(n), hp(25), attackD(5) {}
+};
+
 int main() { 
     std::string pname;
     std::cout << "Insert Player Name: ";
